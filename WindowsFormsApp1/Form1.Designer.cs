ead2389afc79eb82b0758d298499c58590b7c01e
lekaa <lekaaemails@gmail.com>
2024-02-11 16:09:19 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Frinds_Library
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frinds_Library));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Psychological = new System.Windows.Forms.Button();
+            this.programming = new System.Windows.Forms.Button();
+            this.sport_books = new System.Windows.Forms.Button();
+            this.Literature_books = new System.Windows.Forms.Button();
+            this.science_books = new System.Windows.Forms.Button();
+            this.culture_books = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.calc = new System.Windows.Forms.Button();
+            this.Library = new System.Windows.Forms.Label();
+            this.buy1 = new System.Windows.Forms.CheckBox();
+            this.buy2 = new System.Windows.Forms.CheckBox();
+            this.buy3 = new System.Windows.Forms.CheckBox();
+            this.buy4 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Psychological);
+            this.panel1.Controls.Add(this.programming);
+            this.panel1.Controls.Add(this.sport_books);
+            this.panel1.Controls.Add(this.Literature_books);
+            this.panel1.Controls.Add(this.science_books);
+            this.panel1.Controls.Add(this.culture_books);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(35, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // Psychological
+            // 
+            this.Psychological.BackColor = System.Drawing.Color.LightPink;
+            this.Psychological.ForeColor = System.Drawing.Color.Navy;
+            this.Psychological.Location = new System.Drawing.Point(724, 14);
+            this.Psychological.Name = "Psychological";
+            this.Psychological.Size = new System.Drawing.Size(125, 27);
+            this.Psychological.TabIndex = 5;
+            this.Psychological.Text = "Psychological";
+            this.Psychological.UseVisualStyleBackColor = false;
+            // 
+            // programming
+            // 
+            this.programming.BackColor = System.Drawing.Color.LightPink;
+            this.programming.ForeColor = System.Drawing.Color.Navy;
+            this.programming.Location = new System.Drawing.Point(589, 14);
+            this.programming.Name = "programming";
+            this.programming.Size = new System.Drawing.Size(114, 27);
+            this.programming.TabIndex = 4;
+            this.programming.Text = "programming";
+            this.programming.UseVisualStyleBackColor = false;
+            // 
+            // sport_books
+            // 
+            this.sport_books.BackColor = System.Drawing.Color.LightPink;
+            this.sport_books.ForeColor = System.Drawing.Color.Navy;
+            this.sport_books.Location = new System.Drawing.Point(446, 14);
+            this.sport_books.Name = "sport_books";
+            this.sport_books.Size = new System.Drawing.Size(118, 27);
+            this.sport_books.TabIndex = 3;
+            this.sport_books.Text = "sport books";
+            this.sport_books.UseVisualStyleBackColor = false;
+            // 
+            // Literature_books
+            // 
+            this.Literature_books.BackColor = System.Drawing.Color.LightPink;
+            this.Literature_books.ForeColor = System.Drawing.Color.Navy;
+            this.Literature_books.Location = new System.Drawing.Point(293, 14);
+            this.Literature_books.Name = "Literature_books";
+            this.Literature_books.Size = new System.Drawing.Size(117, 27);
+            this.Literature_books.TabIndex = 2;
+            this.Literature_books.Text = "Literature books";
+            this.Literature_books.UseVisualStyleBackColor = false;
+            // 
+            // science_books
+            // 
+            this.science_books.BackColor = System.Drawing.Color.LightPink;
+            this.science_books.ForeColor = System.Drawing.Color.Navy;
+            this.science_books.Location = new System.Drawing.Point(158, 14);
+            this.science_books.Name = "science_books";
+            this.science_books.Size = new System.Drawing.Size(108, 27);
+            this.science_books.TabIndex = 1;
+            this.science_books.Text = "science books";
+            this.science_books.UseVisualStyleBackColor = false;
+            // 
+            // culture_books
+            // 
+            this.culture_books.BackColor = System.Drawing.Color.LightPink;
+            this.culture_books.ForeColor = System.Drawing.Color.Navy;
+            this.culture_books.Location = new System.Drawing.Point(24, 14);
+            this.culture_books.Name = "culture_books";
+            this.culture_books.Size = new System.Drawing.Size(108, 27);
+            this.culture_books.TabIndex = 0;
+            this.culture_books.Text = "culture books";
+            this.culture_books.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.buy4);
+            this.panel2.Controls.Add(this.buy3);
+            this.panel2.Controls.Add(this.buy2);
+            this.panel2.Controls.Add(this.buy1);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(35, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(872, 344);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.LemonChiffon;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(694, 26);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(155, 114);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(472, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(161, 114);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(255, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 114);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(39, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // calc
+            // 
+            this.calc.BackColor = System.Drawing.Color.LightPink;
+            this.calc.ForeColor = System.Drawing.Color.Navy;
+            this.calc.Location = new System.Drawing.Point(317, 526);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(274, 63);
+            this.calc.TabIndex = 2;
+            this.calc.Text = "Calculate";
+            this.calc.UseVisualStyleBackColor = false;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // Library
+            // 
+            this.Library.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Library.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Library.ForeColor = System.Drawing.Color.LightPink;
+            this.Library.Location = new System.Drawing.Point(287, 27);
+            this.Library.Name = "Library";
+            this.Library.Size = new System.Drawing.Size(367, 52);
+            this.Library.TabIndex = 3;
+            this.Library.Text = "Friends Library";
+            this.Library.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buy1
+            // 
+            this.buy1.AutoSize = true;
+            this.buy1.Location = new System.Drawing.Point(78, 158);
+            this.buy1.Name = "buy1";
+            this.buy1.Size = new System.Drawing.Size(54, 21);
+            this.buy1.TabIndex = 4;
+            this.buy1.Text = "20$";
+            this.buy1.UseVisualStyleBackColor = true;
+            // 
+            // buy2
+            // 
+            this.buy2.AutoSize = true;
+            this.buy2.Location = new System.Drawing.Point(293, 158);
+            this.buy2.Name = "buy2";
+            this.buy2.Size = new System.Drawing.Size(54, 21);
+            this.buy2.TabIndex = 5;
+            this.buy2.Text = "25$";
+            this.buy2.UseVisualStyleBackColor = true;
+            // 
+            // buy3
+            // 
+            this.buy3.AutoSize = true;
+            this.buy3.Location = new System.Drawing.Point(510, 158);
+            this.buy3.Name = "buy3";
+            this.buy3.Size = new System.Drawing.Size(54, 21);
+            this.buy3.TabIndex = 6;
+            this.buy3.Text = "25$";
+            this.buy3.UseVisualStyleBackColor = true;
+            // 
+            // buy4
+            // 
+            this.buy4.AutoSize = true;
+            this.buy4.Location = new System.Drawing.Point(724, 158);
+            this.buy4.Name = "buy4";
+            this.buy4.Size = new System.Drawing.Size(54, 21);
+            this.buy4.TabIndex = 7;
+            this.buy4.Text = "30$";
+            this.buy4.UseVisualStyleBackColor = true;
+            // 
+            // Frinds_Library
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(943, 601);
+            this.Controls.Add(this.Library);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Frinds_Library";
+            this.Text = "Frinds Library";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Literature_books;
+        private System.Windows.Forms.Button science_books;
+        private System.Windows.Forms.Button culture_books;
+        private System.Windows.Forms.Button Psychological;
+        private System.Windows.Forms.Button programming;
+        private System.Windows.Forms.Button sport_books;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.Label Library;
+        private System.Windows.Forms.CheckBox buy4;
+        private System.Windows.Forms.CheckBox buy3;
+        private System.Windows.Forms.CheckBox buy2;
+        private System.Windows.Forms.CheckBox buy1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frinds_Library : Form
+    {
+        public Frinds_Library()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void calc_Click(object sender, EventArgs e)
+        {
+            int sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, total = 0;
+            if (buy1.Checked == true)
+            {
+                sum1 = 20;
+            }
+            if (buy2.Checked == true)
+            {
+                sum2 = 25;
+            }
+            if (buy3.Checked == true) 
+            {
+                sum3 = 25;
+            }
+            if (buy4.Checked == true)
+            {
+                sum4 = 30;
+            }
+            total = sum1 + sum2 + sum3 + sum4;
+            MessageBox.Show("Total Price of your books : " + total);
+        }
+
+    }
+}
